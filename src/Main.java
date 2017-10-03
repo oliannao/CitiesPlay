dafb55c35469242e9f13983828128905d49488c7
oliannao <31596803+oliannao@users.noreply.github.com>
2017-10-03 15:16:38 +0300

    new class java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) {
+		CitiesMap citiesMap = new CitiesMap();
+
+		Set<String> cities = citiesMap.initCities(new File("D:\\1.txt"));
+		citiesMap.createMap(cities);
+
+		Map<Character, Set<String>> hm = citiesMap.createMap(cities);
+
+		/*
+		 * for (Map.Entry<Character, Set<String>> entry : hm.entrySet()) { Character key
+		 * = entry.getKey(); Set value = entry.getValue(); System.out.println(key);
+		 * System.out.println(value);
+		 * 
+		 * }
+		 */
+		Game game = new Game();
+		game.dogame(hm);
+	}
+}
